4f238c4f41c2308d0409b9d08dffb9a129965d9e
NeoHugh <51947522+NeoHugh@users.noreply.github.com>
2020-06-06 13:17:01 +0800

    Add files via upload
    分离了js 新增动态趋势图

--- G7病例监测及发布/static/js/Epi_map.js
+++ G7病例监测及发布/static/js/Epi_map.js
@@ -1,13 +1,13 @@
+//以下是中国疫情地图部分
 var ec_center = echarts.init(document.getElementById("Epi_map"));
 //dummy data
-var dummy_data=[{'name':'上海','value':'1'},{'name':'重庆','value':'23'},{'name':'北京','value':'323'},
-{'name':'海南','value':'1123'},{'name':'西藏','value':'523'},{'name':'台湾','value':'42113'}];
-var dummy_data2=[{'name':'上海','value':'1'},{'name':'重庆','value':'23'},{'name':'北京','value':'3'},
-{'name':'海南','value':'13'},{'name':'西藏','value':'3'},{'name':'台湾','value':'42'}];
+var dummy_data = [{ 'name': '上海', 'value': '1' }, { 'name': '重庆', 'value': '23' }, { 'name': '北京', 'value': '323' },
+{ 'name': '海南', 'value': '1123' }, { 'name': '西藏', 'value': '523' }, { 'name': '台湾', 'value': '42113' }];
+var dummy_data2 = [{ 'name': '上海', 'value': '1' }, { 'name': '重庆', 'value': '23' }, { 'name': '北京', 'value': '3' },
+{ 'name': '海南', 'value': '13' }, { 'name': '西藏', 'value': '3' }, { 'name': '台湾', 'value': '42' }];
 
-var choice=dummy_data;
+var choice = dummy_data;
 var ec_center_option = {
-    backgroundColor:'',
     title: {
         text: '',
         subtext: '',
@@ -28,21 +28,21 @@ var ec_center_option = {
             start: 1,
             end: 9
         },
-            {
-                start: 10,
-                end: 99
-            },
-            {
-                start: 100,
-                end: 999
-            },
-            {
-                start: 1000,
-                end: 9999
-            },
-            {
-                start: 10000
-            },
+        {
+            start: 10,
+            end: 99
+        },
+        {
+            start: 100,
+            end: 999
+        },
+        {
+            start: 1000,
+            end: 9999
+        },
+        {
+            start: 10000
+        },
         ],
         color: ['#8A3310', '#C64918', '#E55B25', '#F2AD92', '#F9DCD1']
     },
@@ -52,7 +52,6 @@ var ec_center_option = {
         type: 'map',
         mapType: 'china',
         roam: false, //拖动和缩放
-        // background:'#ff00ff',
         itemStyle: {
             normal: {
                 borderWidth: .5, //区域边框宽度
@@ -80,24 +79,74 @@ var ec_center_option = {
 };
 ec_center.setOption(ec_center_option);
 //实现点击按钮切换地图类型
-$("#changeMap").click(function (){
-    if (choice==dummy_data)
-    {
-        choice=dummy_data2;
+$("#changeMap").click(function () {
+    if (choice == dummy_data) {
+        choice = dummy_data2;
         $("#changeMap").html('切换累积确诊地图');
     }
-    else{
-        choice=dummy_data;
+    else {
+        choice = dummy_data;
         $("#changeMap").html('切换现存确诊地图')
     }
     ec_center.setOption({
-    series:[{
-            data:choice
-    }]
+        series: [{
+            data: choice
+        }]
     })
-
-
-    
 })
+
+
+//实现点击疫情地图上的省市板块就将对应数据加到趋势图中：
+ec_center.on('click', function (param) {
+    var region = param['name'];
+    var found = 0;
+    for (var index=0;index<optionContrast.series.length;index++) {
+        //点击了已经在图上的省市，将之从折线图里面删去。需要删除legend以及series里对应地区的部分
+        if (region == optionContrast.series[index].name) {
+            found = 1;
+            for (index_next = index; index_next < optionContrast.series.length - 1; index_next++) {
+                optionContrast.series[index_next] = optionContrast.series[index_next + 1];
+            }
+            optionContrast.series.pop();
+            for (index_next = index; index_next < optionContrast.legend.data.length - 1; index_next++) {
+                optionContrast.legend.data[index_next] = optionContrast.legend.data[index_next + 1];
+            }
+            optionContrast.legend.data.pop();
+            contrast.clear();
+            break;
+        }
+    }
+    //点击了不在折线图上的省市,将数据加入折线图
+    if (!found) {
+        var tempList = [];
+        for (const index in provinceGather) {
+            if (provinceGather[index].proName == region) {
+                // contrastLegend.push(region);
+                optionContrast.legend.data.push(region);
+                for (index_date in provinceGather[index].dayListing) {
+                    tempList.push(provinceGather[index].dayListing[index_date].import);
+                }
+                optionContrast.series.push({name:region,type:'line',data:tempList});
+
+            }
+        }
+
+    }
+
+        contrast=echarts.init(document.getElementById('contrastGraph'));
+        contrast.setOption(optionContrast);
+
+    //格式
+    // provinceGather=[{
+    // proName：xxx，dayListing:[{date:'xx', imported:xxx},{},{},{}]
+    //},{},{},{}……]]
+
+})
+
+
+    //格式
+    // provinceGather=[{
+    // proName：xxx，dayListing:[{date:'xx', imported:xxx},{},{},{}]
+    //},{},{},{}……]]
 
 
---
+++ G7病例监测及发布/static/js/statistics.js
@@ -0,0 +1,367 @@
+
+// 初始化各echarts实例
+var trendChart1 = echarts.init(document.getElementById('trendNationWide'));
+var trendChart2 = echarts.init(document.getElementById('trendTotal'));
+var trendChart3 = echarts.init(document.getElementById('trendDaily'));
+
+// 样例数据，等待同步后端的数据。
+// 注意：请确保传回数据中各省份日期序列相同且按时间顺序排列！！
+var data = [{
+    province: "浙江",
+    data: [{
+        date: "03-01", diagnosed: 100, imported: 10,
+        asymptomatic: 15, cured: 5, dead: 1
+    },
+    {
+        date: "03-02", diagnosed: 90, imported: 9,
+        asymptomatic: 20, cured: 10, dead: 2
+    },
+    {
+        date: "03-03", diagnosed: 80, imported: 11,
+        asymptomatic: 17, cured: 15, dead: 0
+    }]
+},
+{
+    province: "江苏",
+    data: [{
+        date: "03-01", diagnosed: 70, imported: 5,
+        asymptomatic: 12, cured: 13, dead: 1
+    },
+    {
+        date: "03-02", diagnosed: 30, imported: 6,
+        asymptomatic: 32, cured: 43, dead: 0
+    },
+    {
+        date: "03-03", diagnosed: 40, imported: 7,
+        asymptomatic: 14, cured: 24, dead: 1
+    }]
+},
+{
+    province: "湖北",
+    data: [{
+        date: "03-01", diagnosed: 70, imported: 3,
+        asymptomatic: 12, cured: 13, dead: 1
+    },
+    {
+        date: "03-02", diagnosed: 30, imported: 2,
+        asymptomatic: 32, cured: 43, dead: 0
+    },
+    {
+        date: "03-03", diagnosed: 40, imported: 1,
+        asymptomatic: 14, cured: 24, dead: 1
+    }]
+}
+];
+// 样例目标省份。可填各省名称、"全国"、"非湖北"
+var targetProvince = "浙江";
+
+
+// 计算全国及目标地区各日数据
+var nationwideData = []; // 绘制用数据
+for (const provinceN in data) {
+    province = data[provinceN];
+    for (const dataN in province.data) {
+        // 计算全国累计治愈、死亡
+        dateData = province.data[dataN];
+        if (provinceN == 0) {
+            nationwideData.push({
+                date: dateData.date,
+                diagnosed: dateData.diagnosed,
+                cured: dateData.cured,
+                dead: dateData.dead
+            });
+        } else {
+            toModifiedData = nationwideData[dataN];
+            toModifiedData.diagnosed += dateData.diagnosed;
+            toModifiedData.cured += dateData.cured;
+            toModifiedData.dead += dateData.dead;
+        }
+    }
+}
+
+// 计算全国治愈率、死亡率趋势
+var nationwideRateData = [];
+var totalDiagnosed = 0, totalDead = 0, totalCured = 0;
+for (const dateData of nationwideData) {
+    totalDiagnosed += dateData.diagnosed;
+    totalCured += dateData.cured;
+    totalDead += dateData.dead;
+    nationwideRateData.push({
+        date: dateData.date,
+        curedRate: (totalCured / totalDiagnosed * 100).toFixed(1),
+        deadRate: (totalDead / totalDiagnosed * 100).toFixed(1)
+    });
+}
+
+// 变量与文字对应关系
+var seriesText1 = {
+    curedRate: "治愈率",
+    deadRate: "死亡率"
+};
+var seriesText2 = {
+    totalDiagnosed: "累计确诊",
+    totalDead: "累计死亡",
+    totalCured: "累计治愈"
+};
+var seriesText3 = {
+    diagnosed: "新增确诊"
+};
+
+// 全国治愈率、死亡率趋势图数据配置
+var optionTC1 = {
+    title: {
+        text: '全国治愈率、死亡率趋势图'
+    },
+    tooltip: {
+        formatter: function (params) {
+            tipString = "日期：" + params.data.date + "<br>";
+            tipString = tipString + seriesText1[params.seriesName] + "：";
+            tipString = tipString + params.data[params.seriesName] + "%";
+            return tipString;
+        },
+        textStyle: {
+            align: 'left'
+        }
+    },
+    legend: {
+        x: "left",
+        y: "bottom",
+        formatter: function (params) {
+            return seriesText1[params];
+        }
+    },
+    xAxis: { type: 'category' },
+    yAxis: {},
+    series: [
+        { type: "line" },
+        { type: "line" }
+    ],
+    dataset: {
+        dimensions: ['date', 'curedRate', 'deadRate'],
+        source: nationwideRateData
+    }
+};
+
+// 使用刚指定的配置项和数据显示图表。
+trendChart1.setOption(optionTC1);
+
+// 计算各省累计、新增数据并显示图表。之所以单独作为函数是因为这一块可能会被反复调用，因此以重复遍历为代价增强复用性。
+function calcTargetData() {
+    // 计算目标地区新增数据
+    var targetData = []; // 绘制用数据
+    var firstProvince = 1; // 判断是否为第一个录入的省份
+    for (const province of data) {
+        if (province.province == targetProvince || targetProvince == "全国" || (targetProvince == "非湖北" && province.province != "湖北")) {
+            for (const dataN in province.data) {
+                dateData = province.data[dataN];
+                if (firstProvince == 1) {
+                    targetData.push({
+                        date: dateData.date,
+                        diagnosed: dateData.diagnosed,
+                        cured: dateData.cured,
+                        dead: dateData.dead
+                    });
+                } else {
+                    toModifiedData = targetData[dataN];
+                    toModifiedData.diagnosed += dateData.diagnosed;
+                    toModifiedData.cured += dateData.cured;
+                    toModifiedData.dead += dateData.dead;
+                }
+            }
+        }
+        firstProvince = 0;
+    }
+    // 计算目标地区累计确诊、死亡、治愈
+    var totalDiagnosed = 0, totalCured = 0, totalDead = 0;
+    for (dateData of targetData) {
+        totalDiagnosed += dateData.diagnosed;
+        totalCured += dateData.cured;
+        totalDead += dateData.dead;
+        dateData.totalDiagnosed = totalDiagnosed;
+        dateData.totalCured = totalCured;
+        dateData.totalDead = totalDead;
+    }
+
+    // 显示目标地区累计确诊、死亡、治愈趋势图
+    var optionTC2 = {
+        title: {
+            text: '地区累计确诊、死亡、治愈趋势图'
+        },
+        tooltip: {
+            formatter: function (params) {
+                tipString = "日期：" + params.data.date + "<br>";
+                tipString = tipString + seriesText2[params.seriesName] + "：";
+                tipString = tipString + params.data[params.seriesName];
+                return tipString;
+            },
+            textStyle: {
+                align: 'left'
+            }
+        },
+        legend: {
+            x: "left",
+            y: "bottom",
+            formatter: function (params) {
+                return seriesText2[params];
+            }
+        },
+        xAxis: { type: 'category' },
+        yAxis: {},
+        series: [
+            { type: "line" },
+            { type: "line" },
+            { type: "line" }
+        ],
+        dataset: {
+            dimensions: ['date', 'totalDiagnosed', 'totalDead', 'totalCured'],
+            source: targetData
+        }
+    };
+    trendChart2.setOption(optionTC2);
+
+    // 显示目标地区每日新增确诊趋势图
+    var optionTC3 = {
+        title: {
+            text: '地区新增确诊趋势图'
+        },
+        tooltip: {
+            formatter: function (params) {
+                tipString = "日期：" + params.data.date + "<br>";
+                tipString = tipString + seriesText3[params.seriesName] + "：";
+                tipString = tipString + params.data[params.seriesName];
+                return tipString;
+            },
+            textStyle: {
+                align: 'left'
+            }
+        },
+        legend: {
+            x: "left",
+            y: "bottom",
+            formatter: function (params) {
+                return seriesText3[params];
+            }
+        },
+        xAxis: { type: 'category' },
+        yAxis: {},
+        series: [
+            { type: "line" }
+        ],
+        dataset: {
+            dimensions: ['date', 'diagnosed'],
+            source: targetData
+        }
+    };
+    trendChart3.setOption(optionTC3);
+}
+calcTargetData();
+//modify
+//趋势图部分
+var contrast = echarts.init(document.getElementById('contrastGraph'));
+var contrastLegend = ['湖北'], contrastXaxis = [];
+var contrastSeries = [];
+var provinceGather = [];
+for (const index in data) {
+    var proName = data[index].province;
+    //contrastLegend.push(proName);
+    var dayListingThisProvince = [];
+    for (const everyday in data[index].data) {
+        dayListingThisProvince.push(
+            {
+                date: data[index].data[everyday].date,
+                import: data[index].data[everyday].imported
+            }
+        )
+    }
+    provinceGather.push(
+        {
+            proName: proName,
+            dayListing: dayListingThisProvince
+        }
+    )
+}
+
+var tempList = [];
+for (const index in provinceGather) {
+    if (provinceGather[index].proName == '湖北') {
+        for (const index_date in provinceGather[index].dayListing) {
+            tempList.push(provinceGather[index].dayListing[index_date].import);
+            contrastXaxis.push(provinceGather[index].dayListing[index_date].date);
+        }
+        contrastSeries.push({
+            name: '湖北',
+            type: 'line',
+            data: tempList}
+        );
+    }
+}
+
+optionContrast = {
+    title: {
+        text: '地区每日境外输入趋势图'
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data: contrastLegend,
+        x:"left",
+        y:"bottom"
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis: {
+        type: 'category',
+        data: contrastXaxis
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: contrastSeries
+};
+contrast.setOption(optionContrast);
+
+
+//modify
+
+// 当画面大小变化时对应改变各图表的尺寸。
+function resizeCharts() {
+    $("#trendNationWide").css({
+        "height": 0.6 * $("#trendNationWideDiv").width(),
+        "width": $("#trendNationWideDiv").width()
+    });
+    trendChart1.resize();
+    $("#trendTotal").css({
+        "height": 0.6 * $("#trendTotalDiv").width(),
+        "width": $("#trendTotalDiv").width()
+    });
+    trendChart2.resize();
+    $("#trendDaily").css({
+        "height": 0.6 * $("#trendDailyDiv").width(),
+        "width": $("#trendDailyDiv").width()
+    });
+    trendChart3.resize();
+    $("#contrastGraph").css({
+        "height": 0.6 * $("#contrastWrapper").width(),
+        "width": $("#contrastWrapper").width()
+
+    })
+    contrast.resize();
+}
+
+
+
+
+// 不要忘记设置图表的初始大小。
+$(document).ready(function () {
+    resizeCharts();
+})
+
+// 使图表大小进行自适应。
+window.onresize = function () {
+    resizeCharts();
+}
